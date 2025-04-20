5bb23955e0e4b27c0d1093058a0170dc5bf41673
Abel <se.abel.tadesse@gmail.com>
2025-04-20 23:12:40 +0300

    Refactor chef routes to use updated authentication middleware

--- backend/routes/chefRoutes.js
+++ backend/routes/chefRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
 
-const chef_router = express.Router();
-// import { protect, authorizeRoles } from "../middleware/auth.js";
+import { protect, authorizeRoles } from '../config/authMiddleware.js';
 import {  updateStockItem, getStock } from "../controllers/chefController.js";
+
+const chef_router = express.Router();
+
+// Middleware: Only Admins can access these routes
+chef_router.use(protect, authorizeRoles("Executive Chef"));
 
 chef_router.get("/stock",   getStock);
 chef_router.put("/stock/:id",   updateStockItem);
